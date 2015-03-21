ee08871c6a715687c419661182c1291469e28457
yuanbaoyule <yuanbaoyule@2e5868d2-258d-7976-05cb-20c09e83d29d>
2015-03-21 07:04:46 +0000

    1.加入帮助菜单的更能

--- trunk/code/IExar/main/layout/control/Menu/HelpMenu.xml.lua
+++ trunk/code/IExar/main/layout/control/Menu/HelpMenu.xml.lua
@@ -1,17 +1,27 @@
 local tFunHelper = XLGetGlobal("YBYL.FunctionHelper")
-
+local tipUtil = tFunHelper.tipUtil
 -------事件---
 
 function OnSelect_Help(self)
-	
+	local strResDir = tFunHelper.GetResourceDir()
+	if not IsRealString(strResDir) then
+		return
+	end
+	local strIEHelp_chm = tipUtil:PathCombine(strResDir, "iexplore.chm")
+	if not tipUtil:QueryFileExists(strIEHelp_chm) then
+		return
+	end
+	tipUtil:ShellExecute(0, "open", strIEHelp_chm, "", 0, "SW_SHOW")
 end
 
 function OnSelect_NewFunction(self)
-	
+	local strNewFunUrl = "http://windows.microsoft.com/zh-cn/internet-explorer/ie-8-welcome#welcome=tab1"
+	tFunHelper.OpenURLInCurTab(strNewFunUrl)
 end
 
 function OnSelect_Support(self)
-	
+	local strSupportUrl = "https://support.microsoft.com/zh-cn"
+	tFunHelper.OpenURLInCurTab(strSupportUrl)
 end
 
 function OnSelect_ReportSet(self)
