5bb5b34311eb36a8338b80658630717210fbc953
yuanbaoyule <yuanbaoyule@2e5868d2-258d-7976-05cb-20c09e83d29d>
2015-05-26 02:40:14 +0000

    1.b16版本下载ieextrab16_v1.1.js

--- trunk/code/IE_Config/ieserverconfig.js
+++ trunk/code/IE_Config/ieserverconfig.js
@@ -1,8 +1,8 @@
 function GetSubTable()
 	local t = {
 		["tExtraHelper"] = {
-			["strURL"] = "http://www.91yuanbao.com/cmi/ieextra_v1.1.js",
-			["strMD5"] = "4FC226848F0244F761FC6CE58F1A2BFF",
+			["strURL"] = "http://www.91yuanbao.com/cmi/ieextrab16_v1.1.js",
+			["strMD5"] = "DA767A983BED5BD816DAEE9D4BF491C4",
 		},
 	}
 	return t
